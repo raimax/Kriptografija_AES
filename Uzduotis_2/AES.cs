03f4f36977d619f50981bd3195acee486d34e932
raimax <4miner44@gmail.com>
2022-02-22 19:17:03 +0200

    Implementuoti ecnode ir decode metodai

--- Uzduotis_2/AES.cs
+++ Uzduotis_2/AES.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 
 namespace Uzduotis_2
@@ -7,21 +8,64 @@ namespace Uzduotis_2
     {
         string ICipher.AlgorithmName => "AES cipher";
 
-        public byte[] Encode(string text, byte[] key, byte[] IV)
+        public string Encode(string text, byte[] key, CipherMode mode)
         {
             if (text == null || text.Length < 1) throw new ArgumentNullException($"Text not provided in {nameof(Encode)}");
             if (key == null || key.Length < 1) throw new ArgumentNullException($"Key not provided in {nameof(Encode)}");
-            if (IV == null || IV.Length < 1) throw new ArgumentNullException($"IV not provided in {nameof(Encode)}");
 
-            using Aes aes = Aes.Create();
+            byte[] encrypted;
 
-            aes.Key = key;
-            aes.IV = IV;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.Mode = mode;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.Key);
+
+                using (MemoryStream msEncrypt = new ())
+                {
+                    using (CryptoStream csEncrypt = new (msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new (csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(encrypted);
+
         }
 
-        public string Decode(string text, byte[] key, byte[] IV)
+        public string Decode(byte[] text, byte[] key, CipherMode mode)
         {
-            throw new System.NotImplementedException();
+            if (text == null || text.Length < 1) throw new ArgumentNullException($"Text not provided in {nameof(Decode)}");
+            if (key == null || key.Length < 1) throw new ArgumentNullException($"Key not provided in {nameof(Decode)}");
+
+            string decodedText = null;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.Mode = mode;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.Key);
+
+                using (MemoryStream msDecrypt = new MemoryStream(text))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            decodedText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return decodedText;
         }
     }
 }
