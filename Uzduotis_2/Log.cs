f7034d155999101b96a8e294e266a78f48cb157a
raimax <4miner44@gmail.com>
2022-02-23 00:14:50 +0200

    Pridėta klasė informacijos žinutėms atvaizduoti

---
+++ Uzduotis_2/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uzduotis_2
+{
+    public static class Log
+    {
+        public static void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void Info(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
